2096c6c1357665988f53e094a822f221e022f73e
Alberto del Real <theredsantar@gmail.com>
2025-12-07 01:48:18 +0100

    day 2 part 1 done

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -13,16 +13,107 @@ struct Day02: Day {
     }
 
     func part1() -> String {
-        
-        let lines = input.split(separator: ",")
-        
-        
-        print(lines[0].split(separator: "-"))
-        
-        return ""
+
+        var count = 0
+        let lines = input.split { $0 == "," || $0 == "\n" }
+
+        // Loop to get intervals
+        for line in lines {
+            let intervals = line.split(separator: "-")
+
+            // Get first and second elements of the interval
+            guard intervals.count == 2,
+                let start = Int(intervals[0]),
+                let end = Int(intervals[1])
+            else {
+                print("Invalid interval", line)
+                continue
+            }
+
+            var iterValue = start
+
+            // Iterate from start to end
+            while iterValue <= end {
+
+                let numStr = String(iterValue)
+                let numDigits = String(iterValue).count
+                let splitPos = numDigits / 2
+
+                // Check if number has pair digits
+                if numDigits % 2 == 0 {
+                    let index = numStr.index(
+                        numStr.startIndex,
+                        offsetBy: splitPos
+                    )
+
+                    // Split number in two parts
+                    let firstPart = Int(numStr[..<index])
+                    let secondPart = Int(numStr[index...])
+                    // print("\(firstPart), \(secondPart)")
+
+                    // Both parts identical then is a invalid ID
+                    if firstPart == secondPart {
+                        count += iterValue
+                    }
+                }
+                iterValue += 1
+            }
+        }
+
+        let result = String(count)
+        return result
     }
 
     func part2() -> String {
-        return "Pending"
+        var count = 0
+        let lines = input.split { $0 == "," || $0 == "\n" }
+
+        // Loop to get intervals
+        for line in lines {
+            let intervals = line.split(separator: "-")
+
+            // Get first and second elements of the interval
+            guard intervals.count == 2,
+                let start = Int(intervals[0]),
+                let end = Int(intervals[1])
+            else {
+                print("Invalid interval", line)
+                continue
+            }
+            // print("[\(start), \(end)]")
+
+            var iterValue = start
+
+            // Iterate from start to end
+            while iterValue <= end {
+
+                let numStr = String(iterValue)
+                let numDigits = String(iterValue).count
+                let splitPos = numDigits / 2
+
+                // Check if number has pair digits
+                if numDigits % 2 == 0 {
+                    let index = numStr.index(
+                        numStr.startIndex,
+                        offsetBy: splitPos
+                    )
+
+                    // Split number in two parts
+                    let firstPart = Int(numStr[..<index])
+                    let secondPart = Int(numStr[index...])
+                    // print("\(firstPart), \(secondPart)")
+
+                    // Both parts identical then is a invalid ID
+                    if firstPart == secondPart {
+                        count += iterValue
+                        //print("Invalid ID: \(iterValue)")
+                    }
+                }
+                iterValue += 1
+            }
+        }
+
+        let result = String(count)
+        return result
     }
 }
