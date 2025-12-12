18f3dbacf9daa90f354dc28fdf595a50ef8b9e30
Alberto del Real <theredsantar@gmail.com>
2025-12-12 15:07:09 +0100

    AoC tree image added

---
+++ Sources/Day05.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+struct Day05: Day {
+    let day = 5
+    let input: String
+
+    init() {
+        if let inputData = InputReader.read(day: day) {
+            self.input = inputData
+        } else {
+            self.input = ""
+        }
+    }
+
+    func part1() -> String {
+
+        
+        let result = String("WIP")
+        return result
+    }
+
+    func part2() -> String {
+
+        let result = String("WIP")
+        return result
+    }
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -5,9 +5,10 @@ let days: [Day] = [
     Day02(),
     Day03(),
     Day04(),
+    Day05()
 ]
 
-let currentDay = 4
+let currentDay = 5
 
 //days.forEach { $0.run() }
 days[currentDay - 1].run()
